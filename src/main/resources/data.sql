bcdd50a73713975c6b7d748c21f994126f04f22b
Chris Allen <chris.allen070@gmail.com>
2019-10-22 13:59:59 -0400

    Added Swagger2 documentation

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO TBL_EMPLOYEES (first_name, last_name, email) VALUES
-  ('Lokesh', 'Gupta', 'abc@gmail.com'),
-  ('Deja', 'Vu', 'xyz@email.com'),
-  ('Caption', 'America', 'cap@marvel.com');

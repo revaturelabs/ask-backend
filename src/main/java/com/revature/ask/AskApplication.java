c9d9b510f499b2385d04e40837820a491f3c6895
AdamDKing <adamdentonking@gmail.com>
2019-10-22 11:23:18 -0400

    initial project structure (from Chris Allen)

---
+++ src/main/java/com/revature/ask/AskApplication.java
@@ -0,0 +1,13 @@
+package com.revature.ask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/ask/AskApplicationTests.java
@@ -0,0 +1,16 @@
+package com.revature.ask;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AskApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

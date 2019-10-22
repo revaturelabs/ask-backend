2761cb00b0dc958fa626c4043c17c69e134aa682
dblitz <blitzy2k@gmail.com>
2019-10-22 14:11:38 -0400

    tags started

---
+++ src/main/java/com/revaturelabs/ask/tags/Tag.java
@@ -0,0 +1,86 @@
+package com.revaturelabs.ask.tags;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tags")
+public class Tag implements Serializable {
+
+	private static final long serialVersionUID = -7136050878066116590L;
+	
+	@Id
+	private Integer id;
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((tagName == null) ? 0 : tagName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Tag other = (Tag) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (tagName == null) {
+			if (other.tagName != null)
+				return false;
+		} else if (!tagName.equals(other.tagName))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Tag [id=" + id + ", tagName=" + tagName + "]";
+	}
+
+	public Tag() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Tag(Integer id, String tagName) {
+		super();
+		this.id = id;
+		this.tagName = tagName;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTagName() {
+		return tagName;
+	}
+
+	public void setTagName(String tagName) {
+		this.tagName = tagName;
+	}
+
+	@Column
+	private String tagName;
+	
+	
+}
---
+++ src/main/java/com/revaturelabs/ask/tags/TagRepo.java
@@ -0,0 +1,7 @@
+package com.revaturelabs.ask.tags;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TagRepo extends CrudRepository<Tag, Integer> {
+
+}

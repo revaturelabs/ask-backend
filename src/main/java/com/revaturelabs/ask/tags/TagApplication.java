e6623de42eeb30bac702aaf5fa7d081511d6207f
dblitz <blitzy2k@gmail.com>
2019-10-23 13:03:52 -0400

    Hitting the endpoints

--- src/main/java/com/revaturelabs/ask/tags/TagApplication.java
+++ src/main/java/com/revaturelabs/ask/tags/TagApplication.java
@@ -2,9 +2,7 @@ package com.revaturelabs.ask.tags;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
 @SpringBootApplication
 public class TagApplication {
 

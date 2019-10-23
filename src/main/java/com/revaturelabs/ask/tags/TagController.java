1c8c64265671c928f751a58344c32be549de98c2
dblitz <blitzy2k@gmail.com>
2019-10-23 13:11:38 -0400

    Added individual id path

--- src/main/java/com/revaturelabs/ask/tags/TagController.java
+++ src/main/java/com/revaturelabs/ask/tags/TagController.java
@@ -3,11 +3,14 @@ package com.revaturelabs.ask.tags;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping(path = "/tags")
@@ -26,5 +29,15 @@ public class TagController {
 //        //return TagService.create(tag);
 //        return null;
 //    }
+    
+    @GetMapping("/{id}")
+    public Tag getTagById(@PathVariable int id) {
+        try {
+            return tagService.getById(id);
+        } catch (TagNotFoundException e) {
+            
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Tag not found",e);
+        }
+    }
 	
 }

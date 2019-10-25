f97aac5cebce41dbacce9ff30ee88a41b40b21b2
Chris Allen <chris.allen070@gmail.com>
2019-10-25 14:18:21 -0400

    Removed Spring Data Rest from the project

--- src/main/java/com/revaturelabs/ask/tags/TagRepository.java
+++ src/main/java/com/revaturelabs/ask/tags/TagRepository.java
@@ -1,7 +1,6 @@
 package com.revaturelabs.ask.tags;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 @Repository

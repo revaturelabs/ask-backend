53cbb246ad29ad44ee410a630e08dc25be045554
Chris Allen <chris.allen070@gmail.com>
2019-10-25 15:24:03 -0400

    Added cors filtering for the local profile

--- src/main/java/com/revaturelabs/ask/AskApplication.java
+++ src/main/java/com/revaturelabs/ask/AskApplication.java
@@ -3,6 +3,10 @@ package com.revaturelabs.ask;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -17,12 +21,26 @@ public class AskApplication {
   }
 
   /**
-  * Produces the Swagger 2 Docket Bean for the apis in com.revaturelabs.ask
-  */
+   * Produces the Swagger 2 Docket Bean for the apis in com.revaturelabs.ask
+   */
   @Bean
   public Docket productApi() {
     return new Docket(DocumentationType.SWAGGER_2).select()
         .apis(RequestHandlerSelectors.basePackage("com.revaturelabs.ask")).build();
 
+  }
+
+  /**
+   * Provides Cors filtering for the local Spring Boot profile
+   */
+  @Bean
+  @Profile("dev")
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurerAdapter() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+      }
+    };
   }
 }

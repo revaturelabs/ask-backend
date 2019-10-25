10b4c87b24c948b3248f4475ce49b29b9bd012e8
Bryan <BryanFRitt@HotMail.com>
2019-10-25 15:46:36 -0400

    added in some javadoc

--- src/main/java/com/revaturelabs/ask/response/ResponseController.java
+++ src/main/java/com/revaturelabs/ask/response/ResponseController.java
@@ -15,6 +15,14 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+/**
+ * The ResponseController is responsible for handling responses to questions. Can return a list of
+ * responses, a response by id, and add response to database and update a response. Responses are in
+ * JSON format.
+ * 
+ * @author Bryan Ritter
+ *
+ */
 @RestController
 @RequestMapping(path = "/response")
 public class ResponseController {
@@ -22,11 +30,22 @@ public class ResponseController {
   @Autowired
   ResponseService responseService;
 
+  /**
+   * Accepts HTTP GET request. Returns a list of all responses on the database as a JSON object
+   * 
+   * @return a List of Responses that contain all responses on the database
+   */
   @GetMapping
   public List<Response> getAllResponses() {
     return this.responseService.getAll();
   }
 
+  /**
+   * Accepts HTTP GET request. Returns a Response instance as a JSON entity based on the given id.
+   * 
+   * @param id receives the id of a response
+   * @return response entity which has the same id as the given id.
+   */
   @GetMapping("/{id}")
   public Response getResponseById(@PathVariable int id) {
     try {
@@ -36,33 +55,64 @@ public class ResponseController {
     }
   }
 
+  /**
+   * Accepts a HTTP POST request. Attempts to add a Response instance to the database
+   * 
+   * @param response receives a response object
+   * @return a Response object
+   */
   @PostMapping
   public Response createResponse(@RequestBody Response response) {
     return this.responseService.create(response);
   }
 
+  /**
+   * Accepts HTTP PUT requests. Takes in a response and updates any matching response in the
+   * database. If no response on the database has a matching id, then the given response is added to
+   * the database.
+   * 
+   * @param response the response to create
+   * @param id the id of the response to create
+   * @return
+   */
   @PutMapping("/{id}")
   public Response createOrUpdate(@RequestBody Response response, @PathVariable int id) {
     response.setId(id);
     try {
       return this.responseService.createOrUpdate(response);
     } catch (ResponseConflictException e) {
-      throw new ResponseStatusException(HttpStatus.CONFLICT, "Response already exists", e);
+      throw new ResponseStatusException(HttpStatus.CONFLICT,
+          "Error during update/creation of response", e);
     }
   }
 
+  /**
+   * Accepts HTTP PUT requests. Takes in a response and updates any matching response in the
+   * database. If no response on the database has a matching id, then the given response is added to
+   * the database.
+   * 
+   * @param response the response to update
+   * @param id the id of the response to update
+   * @return ResponseService
+   */
   @PatchMapping("/{id}")
   public Response updateResponse(@RequestBody Response response, @PathVariable int id) {
     response.setId(id);
     try {
       return this.responseService.update(response);
     } catch (ResponseConflictException e) {
-      throw new ResponseStatusException(HttpStatus.CONFLICT, "Response already exists", e);
+      throw new ResponseStatusException(HttpStatus.CONFLICT, "Error during update of response", e);
     } catch (ResponseNotFoundException e) {
       throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Response not found", e);
     }
   }
 
+  /**
+   * Accepts HTTP DELETE requests. Takes in a response id, and deletes the corresponding response,
+   * if it exists.
+   * 
+   * @param id
+   */
   @DeleteMapping("/{id}")
   @ResponseStatus(HttpStatus.NO_CONTENT)
   public void deleteResponse(@PathVariable int id) {
@@ -73,3 +123,5 @@ public class ResponseController {
     }
   }
 }
+
+
--- src/main/java/com/revaturelabs/ask/response/ResponseServiceImpl.java
+++ src/main/java/com/revaturelabs/ask/response/ResponseServiceImpl.java
@@ -6,17 +6,36 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
+/**
+ * Service class for managing responses. It contains methods for finding all responses, finding a
+ * response by id, creating a response, and updating.
+ * 
+ * @author Bryan Ritter
+ *
+ */
 @Service
 public class ResponseServiceImpl implements ResponseService {
 
   @Autowired
   ResponseRepository responseRepository;
 
+  /**
+   * Returns a list of all response on the database
+   * 
+   * @return a List of Responses that contains all responses on the database.
+   */
   @Override
   public List<Response> getAll() {
     return (List<Response>) this.responseRepository.findAll();
   }
 
+
+  /**
+   * Returns responses that match the given id.
+   * 
+   * @param id
+   * @return Response all response that match the given id. empty if no responses match
+   */
   @Override
   public Response getById(int id) throws ResponseNotFoundException {
     Optional<Response> response = this.responseRepository.findById(Integer.valueOf(id));
@@ -28,11 +47,25 @@ public class ResponseServiceImpl implements ResponseService {
     return response.get();
   }
 
+
+
+  /**
+   * Adds to the database an instance of Response, before adding to database the instance id is set
+   * to zero to allow the database to auto-generate the id of the new added instance.
+   * 
+   * @param response receives a response object
+   */
   @Override
   public Response create(Response response) {
     return this.responseRepository.save(response);
   }
 
+  /**
+   * Takes a Response object and updates any matching entity in the database if no entities match
+   * the question will be created.
+   * 
+   * @param response receives a response object
+   */
   @Override
   public Response update(Response response) {
     Optional<Response> existingResponse =
@@ -43,7 +76,7 @@ public class ResponseServiceImpl implements ResponseService {
       try {
         updatedResponse = this.responseRepository.save(response);
       } catch (DataIntegrityViolationException e) {
-        throw new ResponseConflictException("Response name already exists");
+        throw new ResponseConflictException("Error updating response");
       }
     } else {
       throw new ResponseNotFoundException("Unable to find response to update");
@@ -52,6 +85,12 @@ public class ResponseServiceImpl implements ResponseService {
     return updatedResponse;
   }
 
+  /**
+   * Takes in a Response object and either edits an existing Response object or creates a new
+   * response object
+   * 
+   * @param response response to update or create
+   */
   @Override
   public Response createOrUpdate(Response response) {
     Response updatedResponse = null;
@@ -64,6 +103,11 @@ public class ResponseServiceImpl implements ResponseService {
     return updatedResponse;
   }
 
+  /**
+   * Takes in an id of a response to delete and deletes it
+   * 
+   * @param id an id of a response to be deleted
+   */
   @Override
   public void delete(int id) {
     boolean categoryExists = this.responseRepository.existsById(Integer.valueOf(id));

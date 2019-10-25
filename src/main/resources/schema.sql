c969630f4cfeae892d38a679321e4baeabb7474f
Adam <36601431+AdamDKing@users.noreply.github.com>
2019-10-25 16:42:16 -0400

    Update schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,13 +1,3 @@
-DROP TABLE IF EXISTS questions;
-
-CREATE TABLE questions (
-	id SERIAL PRIMARY KEY,
-	user_Id Integer ,
-	head varchar(100),
-	body varchar(100),
-	creation_date Date NOT null
-);
-
 DROP TABLE IF EXISTS tags;
 DROP TABLE IF EXISTS questions;
 DROP TABLE IF EXISTS responses;

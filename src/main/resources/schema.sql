353c04811493efb38bc3deff0cdf2d9da4e2a4fb
Carlos Santos <carlosasantos17@gmail.com>
2019-10-29 10:23:26 -0400

    Restored data to sql scripts.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -8,3 +8,31 @@ CREATE TABLE USERS (
 	password VARCHAR,
 	expert BOOLEAN
 );
+
+DROP TABLE IF EXISTS tags;
+
+DROP TABLE IF EXISTS questions;
+DROP TABLE IF EXISTS responses;
+  
+CREATE TABLE tags (
+  id INT AUTO_INCREMENT  PRIMARY KEY,
+  tag_name VARCHAR(30) UNIQUE
+);
+
+
+CREATE TABLE questions (
+  id SERIAL PRIMARY KEY,
+  user_Id Integer ,
+  head varchar(140),
+  body varchar(1000),
+  poststamp Date NOT null
+);
+
+
+CREATE TABLE responses (
+  id SERIAL PRIMARY KEY,
+  responder_Id Integer, 
+  title varchar(140),
+  body varchar(1000),
+  creation_date Date NOT null
+);

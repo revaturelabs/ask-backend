53275c13224e56df83cc2d495cbc5ecf69fb1a52
dblitz <blitzy2k@gmail.com>
2019-10-31 15:42:43 -0400

    Added some Mockito tests for Lists of tags, Creation, and updates

--- src/test/java/com/revaturelabs/ask/AskApplicationTests.java
+++ src/test/java/com/revaturelabs/ask/AskApplicationTests.java
@@ -2,6 +2,8 @@ package com.revaturelabs.ask;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 import com.revaturelabs.ask.response.Response;
 import com.revaturelabs.ask.response.ResponseController;
 import com.revaturelabs.ask.response.ResponseService;
+import com.revaturelabs.ask.tags.Tag;
+import com.revaturelabs.ask.tags.TagController;
+import com.revaturelabs.ask.tags.TagService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -23,8 +28,14 @@ public class AskApplicationTests {
   @MockBean
   ResponseService responseServiceMock;
 
+  @MockBean
+  TagService tagServiceMock;
+
   @Autowired
   ResponseController responseControllerImpl;
+
+  @Autowired
+  TagController tagControllerImpl;
 
   @Test
   public void testGetResponseById() {
@@ -34,4 +45,55 @@ public class AskApplicationTests {
 
     assertEquals(exampleResponse, responseControllerImpl.getResponseById(1));
   }
+
+  @Test
+  public void testGetTagById() {
+    Tag exampleTag = new Tag();
+    when((tagServiceMock.getById(1))).thenReturn(exampleTag);
+    assertEquals(exampleTag, tagControllerImpl.getTagById(1));
+  }
+
+  @Test
+  public void testGetAllTags() {
+    Tag javaScriptTag = new Tag();
+    javaScriptTag.setName("JavaScript");
+    Tag javaTag = new Tag();
+    javaTag.setName("Java");
+    List<Tag> exampleTags = new ArrayList();
+    exampleTags.add(javaScriptTag);
+    exampleTags.add(javaTag);
+    when((tagServiceMock.getAll())).thenReturn(exampleTags);
+    assertEquals(exampleTags, tagControllerImpl.getAllTags());
+  }
+
+  @Test
+  public void testCreate() {
+    Tag exampleTag = new Tag();
+    exampleTag.setName("JavaScript");
+    when((tagServiceMock.create(exampleTag))).thenReturn(exampleTag);
+    assertEquals(exampleTag, tagControllerImpl.createTag(exampleTag));
+  }
+
+  @Test
+  public void testUpdate() {
+    Tag exampleTag = new Tag();
+    exampleTag.setId(1);
+    exampleTag.setName("JavaScript");
+
+    Tag exampleTag2 = new Tag();
+    exampleTag2.setName("JScript");
+
+    when((tagServiceMock.update(exampleTag))).thenReturn(exampleTag);
+    assertEquals(exampleTag, tagControllerImpl.updateTag(exampleTag, 1));
+  }
+
+  @Test
+  public void testCreateUpdate() {
+    Tag exampleTag = new Tag();
+    exampleTag.setName("JavaScript");
+    when((tagServiceMock.createOrUpdate(exampleTag))).thenReturn(exampleTag);
+    assertEquals(exampleTag, tagControllerImpl.createOrUpdate(exampleTag, 1));
+  }
+
+
 }

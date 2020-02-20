dd675e25f5a24b6e9bacfc72d3562b5b7945ba0c
Jordan Adkins <36913671+JordanAdkins@users.noreply.github.com>
2020-02-20 10:32:56 -0500

    Fixed Functionality for AMA

---
+++ src/main/java/com/revaturelabs/chat/ChatController.java
@@ -0,0 +1,47 @@
+package com.revaturelabs.chat;
+
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.Map;
+
+@RestController
+@RequestMapping(value = "/api/socket")
+@CrossOrigin
+public class ChatController {
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+	
+    @MessageMapping("/send/message")
+    public Map<String, String> broadcastNotification(String message){
+    	
+    	System.out.println(message);
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, String> messageConverted = null;
+        try {
+            messageConverted = mapper.readValue(message, Map.class);
+        } catch (IOException e) {
+            messageConverted = null;
+        }
+        if(messageConverted!=null){
+            if(messageConverted.containsKey("toId") && messageConverted.get("toId")!=null && !messageConverted.get("toId").equals("")){
+                this.simpMessagingTemplate.convertAndSend("/socket-publisher/"+messageConverted.get("toId"),messageConverted);
+                this.simpMessagingTemplate.convertAndSend("/socket-publisher/"+messageConverted.get("fromId"),message);
+            }else{
+                this.simpMessagingTemplate.convertAndSend("/socket-publisher",messageConverted);
+            }
+        }
+        return messageConverted;
+    }
+
+}
